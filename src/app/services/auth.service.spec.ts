656c6aca005f6a665c8900d8a1c88cb7f2f0ebd2
juliane-dev0-0 <juhjuju030@gmail.com>
2025-05-17 19:59:44 -0300

    implementação do keycloack

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
 
-describe('AuthService', () => {
+RTCSessionDescriptionbe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
